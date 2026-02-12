ad80100bd773c05503c6452bfa251529cc63b01e
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2026-02-11 23:49:56 -0600

    feat: add Call and Project models to the module exports

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from models.user import User, RoleEnum
+from models.call import Call
+from models.project import Project
 
-__all__ = ["db", "User", "RoleEnum"]
+__all__ = ["db", "User", "RoleEnum", "Call", "Project"]
