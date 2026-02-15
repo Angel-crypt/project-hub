2d63f9cbf859f22b540d5ea8dc0226b59ba1edae
Angel-crypt <angel.cruz.030905@gmail.com>
2026-02-15 13:15:37 -0600

    feat: add default admin user creation logic in app context

--- index.py
+++ index.py
@@ -1,10 +1,29 @@
 from app import create_app
-from models import db
+from models import db, User, RoleEnum
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
+    
+    # Crear admin por defecto si no existe
+    admin = User.query.filter_by(enrrollment_number="ADMIN").first()
+    if not admin:
+        try:
+            admin = User(
+                enrrollment_number="ADMIN",
+                name="Administrador",
+                password="admin123",
+                role=RoleEnum.ADMIN
+            )
+            db.session.add(admin)
+            db.session.commit()
+            print("Usuario administrador creado:")
+        except Exception as e:
+            db.session.rollback()
+            print(f"Error al crear admin: {e}")
+    else:
+        print("Usuario administrador ya existe")
 
 if __name__ == "__main__":
     app.run(debug=True)
