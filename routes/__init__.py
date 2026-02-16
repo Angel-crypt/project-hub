d28052aed742785f7a3d27a72cfd2270021bb3d7
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2026-02-15 22:04:59 -0600

    feat: add deliverable_bp to routes for managing project deliverables

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,5 +2,6 @@ from .auth_routes import auth_bp
 from .user_routes import user_bp
 from .call_routes import call_bp
 from .project_routes import project_bp
+from .deliverable_routes import deliverable_bp
 
-__all__ = ["auth_bp", "user_bp", "call_bp", "project_bp"]
+__all__ = ["auth_bp", "user_bp", "call_bp", "project_bp", "deliverable_bp"]
