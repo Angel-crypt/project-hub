994d628f15a5dfb11c3ebee478ced11cd9f01d27
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2026-02-15 19:54:29 -0600

    feat: add seeding functionality for calls and update user seeding logic

--- utils/seed.py
+++ utils/seed.py
@@ -1,4 +1,6 @@
-from models import db, User, RoleEnum
+from operator import call
+from models import db, User, RoleEnum, Call
+from datetime import datetime
 
 DEFAULT_USERS = [
     {
@@ -21,9 +23,40 @@ DEFAULT_USERS = [
     },
 ]
 
+DEFAULT_CALLS = [
+    {
+        "title": "Convocatoria de Proyectos 2026",
+        "description": "Abierta para propuestas de proyectos innovadores en tecnología y sostenibilidad.",
+        "opening_date": "2026-01-01",
+        "closing_date": "2026-02-28",
+    },
+    {
+        "title": "Becas de Investigación Q1 2026",
+        "description": "Programa de becas para proyectos de investigación en ciencias aplicadas.",
+        "opening_date": "2026-01-15",
+        "closing_date": "2026-03-15",
+    },
+    {
+        "title": "Hackathon Universitario 2026",
+        "description": "Competencia de desarrollo de software orientada a soluciones educativas.",
+        "opening_date": "2026-02-01",
+        "closing_date": "2026-02-15",
+    },
+]
+
 
 def seed_users():
-    for data in DEFAULT_USERS:
-        if not User.query.filter_by(enrrollment_number=data["enrrollment_number"]).first():
-            db.session.add(User(**data))
+    for user_data in DEFAULT_USERS:
+        if not User.query.filter_by(enrrollment_number=user_data["enrrollment_number"]).first():
+            db.session.add(User(**user_data))
+    
+    db.session.commit()
+
+def seed_calls():
+    for call_data in DEFAULT_CALLS:
+        call_data["opening_date"] = datetime.strptime(call_data["opening_date"], "%Y-%m-%d")
+        call_data["closing_date"] = datetime.strptime(call_data["closing_date"], "%Y-%m-%d")
+        if not Call.query.filter_by(title=call_data["title"]).first():
+            db.session.add(Call(**call_data))
+    
     db.session.commit()
